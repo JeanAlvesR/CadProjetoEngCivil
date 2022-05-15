6293232c7aac87f09bda37c79064beb0837809e9
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-05-15 19:40:01 -0300

    Add entidade cliente

---
+++ ProjCadEngCivil/src/Entidades/Cliente.java
@@ -0,0 +1,109 @@
+package Entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+
+    private Endereco endereco;
+
+    private List<Projeto> projetos = new ArrayList<>();
+
+    public Cliente(){
+        nome = "";
+        cpf = "";
+        email = "";
+        telefone = "";
+        endereco = new Endereco();
+    }
+
+    public Cliente(String nome, String cpf, String email, String telefone, Endereco endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        cpf = cpf.replace(" ", "");
+        cpf = cpf.replace("-","");
+        cpf = cpf.replace(".","");
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Projeto> getProjetos() {
+        return projetos;
+    }
+
+    public void addProjeto(Projeto projeto){
+        projetos.add(projeto);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco=" + endereco +
+                ", projetos=" + projetos +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return cpf.equals(cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+}
--- ProjCadEngCivil/src/Principal/Principal.java
+++ ProjCadEngCivil/src/Principal/Principal.java
@@ -16,7 +16,7 @@ public class Principal {
     public static void main(String[] args) throws ParseException {
 
 
-        
+
 
 
 
